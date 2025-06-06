fc4ab646122244a36ca2d09cb1c2649f64d50303
Gobinda Das <dasgobinda9955@gmail.com>
2025-06-06 18:07:22 +0530

    feat: implement social image upload and download functionality with format selection

--- app/(app)/social-share/page.tsx
+++ app/(app)/social-share/page.tsx
@@ -1,7 +1,90 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
+import { CldImage } from 'next-cloudinary';
+
+
+
+const socialFormats = {
+  "Instagram Square (1:1)": { width: 1080, height: 1080, aspectRatio: "1:1" },
+  "Instagram Portrait (4:5)": { width: 1080, height: 1350, aspectRatio: "4:5" },
+  "Twitter Post (16:9)": { width: 1200, height: 675, aspectRatio: "16:9" },
+  "Twitter Header (3:1)": { width: 1500, height: 500, aspectRatio: "3:1" },
+  "Facebook Cover (205:78)": { width: 820, height: 312, aspectRatio: "205:78" },
+};
+
+type SocialFormat = keyof typeof socialFormats;
 
 
 function SocialShare() {
+
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  const [selectedFormat, setSelectedFormat] = useState<SocialFormat>("Instagram Square (1:1)");
+  const [isUploading, setIsUploading] = useState(false);
+  const [isTransforming, setIsTransforming] = useState(false);
+  const imageRef = useRef<HTMLImageElement>(null);
+
+
+  useEffect(() => {
+    if (uploadedImage) {
+      setIsTransforming(true);
+    }
+  }, [selectedFormat, uploadedImage]);
+
+
+  const handleImageUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+
+      const file = event.target.files?.[0];
+
+      if (!file) return;
+
+      setIsUploading(true);
+
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const response = await fetch("/api/image-upload", {
+        method: "POST",
+        body: formData
+      })
+
+      if (!response.ok) throw new Error("Failed to upload image");
+
+      const data = await response.json();
+      console.log(data)
+      setUploadedImage(data.public_id);
+      
+    } catch (error) {
+      console.error("Failed to uploading image: ", error);
+      alert("Failed to uploading image");
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+
+  const handleDownload = () => {
+
+    if(!imageRef.current) return;
+
+    fetch(imageRef.current.src)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob)
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `${selectedFormat.replace(/\s+/g, "_").toLowerCase()}.png`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(link);
+      })
+  }
+
+
+
   return (
     <div>
       Social share
